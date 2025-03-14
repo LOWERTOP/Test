e9c92ca42f83dc0b343f632b39be45e5f2dc2af8
LOWERTOP <onone.so@gmail.com>
2025-03-14 22:32:50 +0800

    Update AntiAD.js

--- JS/AntiAD.js
+++ JS/AntiAD.js
@@ -4,6 +4,7 @@
  * 目标：
  * - 删除 twmanga & baozimh 广告 class: "mobadsq", "recommend", "footer"
  * - 删除 twmanga.com 的 div[style="overflow:hidden; flex: 1;"]
+ * - 删除 id="download-swiper" 的 div
  * 
  * 作者: LOWERTOP
  */
@@ -16,15 +17,18 @@ if (!$response || !$response.body) {
 
 let modifiedBody = $response.body;
 
-// 统一删除指定的 class 广告元素
+// 统一删除指定 class 广告元素
 const adClasses = ["mobadsq", "recommend", "footer"];
 adClasses.forEach(adClass => {
     const regex = new RegExp(`<div[^>]*class=["']?${adClass}["']?[^>]*>.*?<\\/div>`, "gis");
     modifiedBody = modifiedBody.replace(regex, '');
 });
 
-// 删除指定 style="overflow:hidden; flex: 1;"
+// 删除 div[style="overflow:hidden; flex: 1;"]
 modifiedBody = modifiedBody.replace(/<div[^>]*style=["']?overflow:\s?hidden;\s?flex:\s?1;["']?[^>]*>.*?<\/div>/gis, '');
+
+// **删除 id="download-swiper" 的 div**
+modifiedBody = modifiedBody.replace(/<div[^>]*id=["']?download-swiper["']?[^>]*>.*?<\/div>/gis, '');
 
 // 返回修改后的 HTML
 $done({ body: modifiedBody });
